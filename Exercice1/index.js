27f7473e73c57b3c6d8326d2a429ddf76e31cad6
Luca Billat <lucabillatpro@gmail.com>
2025-02-25 15:14:44 +0100

    done

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,20 @@
 //votre code ici
 
-export default pairNumbers
+function pairNumbers(x, y) {
+  let result = [];
+
+  if (typeof x === "string" && typeof y === "string") {
+    x = parseInt(x);
+    y = parseInt(y);
+  }
+
+  for (let i = x; i <= y; i++) {
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+
+  return result.join(",");
+}
+
+export default pairNumbers;
