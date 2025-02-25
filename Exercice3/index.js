ba2c7776d62144b85322a1d735ff65dddba0eb42
Luca Billat <lucabillatpro@gmail.com>
2025-02-25 15:17:41 +0100

    Done

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,8 +1,19 @@
 // Créez votre fonction ici
 
-// Exemples d'utilisation de la fonction
-console.log(calculateAverage([5, 10, 15])) // retourne 10
-console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
-console.log(calculateAverage()) // No numbers to calculate average
+function calculateAverage(arr) {
+  if (arr === undefined) {
+    return "No numbers to calculate average";
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
 
-export default calculateAverage
+// Exemples d'utilisation de la fonction
+console.log(calculateAverage([5, 10, 15])); // retourne 10
+console.log(calculateAverage([10, 20, 30, 20])); // retourne 20
+console.log(calculateAverage()); // No numbers to calculate average
+
+export default calculateAverage;
